42c5b2b03112ed7ccd073dc4ed4f96dc7fcd43ff
lucabeltrao11-code <lucabeltrao11@gmail.com>
2025-12-04 13:05:07 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from price_fetcher import get_crypto_price_history
+from indicators import analyze_asset
+
+app = FastAPI(title="Sistema de Análise de Mercado")
+
+@app.get("/")
+def home():
+    return {"status": "online", "message": "API de Análise de Mercado funcionando!"}
+
+
+@app.get("/analisar/{cripto}")
+def analisar_cripto(cripto: str, dias: int = 30):
+    """
+    Ex.: /analisar/bitcoin?dias=90
+    """
+    data = get_crypto_price_history(cripto, dias)
+    analise = analyze_asset(data)
+    return analise
